f2e25ee9d19ecd1e306f9fe6bae07768bdd2bc98
Serchacho <83768648+Serchacho@users.noreply.github.com>
2026-03-03 11:22:54 -0500

    almost done. just need to add ID search and dynamic page when adding to team

--- Assignment9-Pokemon/js.js
+++ Assignment9-Pokemon/js.js
@@ -12,9 +12,92 @@ async function searchPokemon(){
 
         const data = await response.json();
         console.log(data);
+        
+        // 1. get sprite
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+
+
+        const imgElement2 = document.getElementById("");
+
+
+
+        // 2. get audio
+        const pokemonAudioUrl = data.cries.latest;
+        const audioElement = document.querySelector("audio"); // Targets the parent audio tag
+
+        audioElement.src = pokemonAudioUrl; // Set the src directly on the audio element
+        audioElement.load(); // Forces the player to load the new file
+
+        // 3. get moves
+        const moves = data.moves;
+
+        const moveSelects = [
+            document.getElementById("move1"),
+            document.getElementById("move2"),
+            document.getElementById("move3"),
+            document.getElementById("move4"),
+        ];
+
+        // look through each dropdown to fill
+        moveSelects.forEach(select => {
+            // clear existing options
+            select.innerHTML = '';
+
+            const defaultOption = document.createElement("option");
+            defaultOption.text = "--- Select a move ---";
+            select.appendChild(defaultOption);
+
+            moves.forEach(moveEntry => {
+                const option = document.createElement("option");
+                option.text = moveEntry.move.name;
+                select.appendChild(option);
+            });
+        });
+
+
+
     }
     catch(error){
         console.error(error);
     }
 
+}
+
+
+
+function addToTeam(){
+    const teamList = document.getElementById("team");
+
+    // get data of pokemon
+    const pokemonSrc = document.getElementById("pokemonSprite").src;
+    const move1 = document.getElementById("move1").value;
+    const move2 = document.getElementById("move2").value;
+    const move3 = document.getElementById("move3").value;
+    const move4 = document.getElementById("move4").value;
+
+    // create container for team member
+    const article = document.createElement("article");
+    article.classList.add("member");
+
+    // add html
+    article.innerHTML = `
+        <img src="${pokemonSrc}" alt="pokemon team member">
+        <ul>
+            <li>${move1}</li>
+            <li>${move2}</li>
+            <li>${move3}</li>
+            <li>${move4}</li>
+        </ul>
+    `
+
+    // add to team list
+    teamList.appendChild(article);
+    
+
+    
 }
